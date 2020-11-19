5c815c6c07518caba2d4fd6988bc56c1f53fe6e0
ross thompson <rossathompson94@gmail.com>
2020-11-19 11:20:12 +0000

    refactored the search component to use hooks instead of being class based

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,35 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-class Search extends React.Component {
-    state = { term: '' }
+const Search = ({ onFormSubmit }) => {
+	
+	const [term, setTerm] = useState('');
 
-    onInputChange = e => {
-        this.setState({ term: e.target.value })
-    }
+	const onInputChange = (e) => {
+		setTerm(e.target.value)
+	}
 
-    onFormSubmit = e => {
-        e.preventDefault();
+	const onSubmit = (e) => {
+		e.preventDefault();
+		onFormSubmit(term);
+	}
 
-        // To do - call callback from parent component. 
-        this.props.onFormSubmit(this.state.term)
-    }
-
-    render () {
-        return (
-            <div className="search-bar ui segment">
-                <form onSubmit={this.onFormSubmit}className="ui form">
-                    <div className="field">
-                        <label>Video Search</label>
-                        <input 
-                            type="text" 
-                            value={this.state.term}
-                            onChange={this.onInputChange}
-                        />
-                    </div>
-                </form>
-            </div>
-        )
-    }
+	return (
+		<div className="search-bar ui segment">
+				<form onSubmit={onSubmit}className="ui form">
+						<div className="field">
+								<label>Video Search</label>
+								<input 
+										type="text" 
+										value={term}
+										onChange={onInputChange}
+								/>
+						</div>
+				</form>
+		</div>
+	)
 }
 
-export default Search;
+export default Search
